2d13924a579982feb49d65957c4d09182058c353
Yuri Silva <yrgamerbr@gmail.com>
2023-05-25 15:49:03 -0300

    reducing code

--- cryptography/src/utils/handlemsg.ts
+++ cryptography/src/utils/handlemsg.ts
@@ -7,25 +7,18 @@ export default function handlemsg(message: string, key: string) {
         letters[String.fromCharCode(i + 65)] = i + 1
     }
 
-    const WHITESPACE_REGEX = /\s/g
     const TWO_DIGIT_REGEX = /\d{2}/g
 
-    const MESSAGE = message.toUpperCase()
+    const MESSAGE_SPLIT = message
+        .toUpperCase()
+        .split('')
 
-    const KEY = key.replace(WHITESPACE_REGEX, '')
-
-    const KEY_DOUBLE_VALUES = KEY.match(TWO_DIGIT_REGEX)?.map(value => parseInt(value))
-
-    const MESSAGE_SPLIT = MESSAGE.split('')
+    const KEY_DOUBLE_VALUES = key.match(TWO_DIGIT_REGEX)?.map(value => parseInt(value))
 
     const MISSING_LENGTH = Math.ceil(MESSAGE_SPLIT.length / 5) * 5 - MESSAGE_SPLIT.length
-    if (MESSAGE_SPLIT.length % 5 !== 0) {
-        for (let j = 0; j < MISSING_LENGTH; j++) {
-            MESSAGE_SPLIT.push(' ')
-        }
-    }
+    const PADDED_MESSAGE = MESSAGE_SPLIT.concat(Array.from({ length: MISSING_LENGTH }, () => ' '))
 
-    const MESSAGE_LETTERS_NUM = MESSAGE_SPLIT.map((letter: string) => letters[letter])
+    const MESSAGE_LETTERS_NUM = PADDED_MESSAGE.map((letter: string) => letters[letter])
 
     return { key: KEY_DOUBLE_VALUES, msg: MESSAGE_LETTERS_NUM, letters: letters }
 }
